ede4478f9be0adcfa9d5ddb658def3d46ff416b1
LucasBoe <lucas@lumotex.de>
2026-03-07 15:17:07 +0100

    added drunk npc animation

--- scripts/npc/modules/animation_module.gd
+++ scripts/npc/modules/animation_module.gd
@@ -11,20 +11,23 @@ const IDLE_PEAK_SHARPNESS = 4
 const WALK_ANIMATION_SPEED = 15.0
 const WALK_ROTATION_STRENGTH = .15
 
+var npc
+
 func _ready():
 	material = material.duplicate(true)
 	
-	var npc = get_parent() as NPC
+	npc = get_parent() as NPC
 	if npc:
 		pass
 		
 	npc.Animator = self
-	randomInstanceOffset = randf() * 1000.0
+	randomInstanceOffset = randf()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	var timeInSeconds = (Time.get_ticks_msec() + randomInstanceOffset) / 1000.0
+	
+	var timeInSeconds = Global.time_now + randomInstanceOffset
 	var target = idle_tween(timeInSeconds)
 	
 	if direction.length() > 0:
@@ -37,7 +40,13 @@ func _process(delta):
 	scale = lerp(scale, target.Scale, lerp)
 
 func walk_tween(timeInSeconds):
-	var t = timeInSeconds * WALK_ANIMATION_SPEED
+	
+	var drunk = 0.0
+	
+	if npc and npc is NPCGuest:
+		drunk = npc.Needs.drunkenness.strength
+	
+	var t = timeInSeconds * WALK_ANIMATION_SPEED / (1.0 + drunk)
 	var raw = pow(abs(sin(t)), .2) * sign(sin(t))
 	
 	direction = direction * Vector2(1,.2)
@@ -46,7 +55,7 @@ func walk_tween(timeInSeconds):
 		xOrientation = sign(direction.x)
 		
 	var dirToRotation = Vector2(1, direction.y * 0.2).angle() * xOrientation
-	var rotationTarget = dirToRotation + raw * WALK_ROTATION_STRENGTH
+	var rotationTarget = dirToRotation + raw * WALK_ROTATION_STRENGTH * lerp(1.0, (sin(t * .667) + 1.0) * 3.0, drunk)
 		
 	var rawS = pow(abs(sin(t)), .4) * sign(sin(t))
 	var scaleBase = abs(rawS) * SQUASH_STRENGTH
