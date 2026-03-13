bd5e03ccb083c07f0b0c275bdc53ff2ac67a74e0
LucasBoe <lucas@lumotex.de>
2026-03-13 10:13:51 +0100

    stairs fixes

--- scripts/room_stairs.gd
+++ scripts/room_stairs.gd
@@ -8,22 +8,29 @@ const GRID_SIZE = 48
 
 func init_room(_x : int, _y : int):
 	super.init_room(_x, _y)
-	refresh_visuals(_x, _y)
+	refresh_visuals()
 
-	var room_below = Global.Building.get_room_from_index(Vector2i(_x, _y - 1))
+	var room_below = Global.Building.get_room_from_index(Vector2i(x, y - 1))
 	if room_below != null:
-		room_below.refresh_visuals(_x, _y - 1)
+		room_below.refresh_visuals()
 
-func refresh_visuals(x, y):
-	var stair_sprite_index = 1;
+	var room_above = Global.Building.get_room_from_index(Vector2i(x, y + 1))
+	if room_above != null:
+		room_above.refresh_visuals()
 
-	if is_basement:
-		if Global.Building.is_bottom_floor(y):
-			stair_sprite_index = 3
-		else:
-			stair_sprite_index = 2
-	elif Global.Building.is_top_floor(y):
+func refresh_visuals():
+	if x == null or y == null:
+		return
+	var has_above = Global.Building.get_room_from_index(Vector2i(x, y + 1)) is RoomStairs
+	var has_below = Global.Building.get_room_from_index(Vector2i(x, y - 1)) is RoomStairs
+
+	var stair_sprite_index: int
+	if not has_above and has_below:
 		stair_sprite_index = 0
+	elif not has_below:
+		stair_sprite_index = 3
+	else:
+		stair_sprite_index = 2 if is_basement else 1
 
 	stairs_background_sprite.region_rect = Rect2(0, GRID_SIZE * stair_sprite_index, GRID_SIZE, GRID_SIZE)
 	stairs_foreground_sprite.region_rect = Rect2(GRID_SIZE, GRID_SIZE * stair_sprite_index, GRID_SIZE, GRID_SIZE)
